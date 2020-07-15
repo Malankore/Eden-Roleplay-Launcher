9e73964a9aa977dc8ab33e19779d77885d0c8c7e
Malankore <tyler@tjeynon.us>
2020-07-15 17:29:29 -0400

    Added some comments for my own sanity and understanding of the flow.

--- ViewModel/MainWindow.cs
+++ ViewModel/MainWindow.cs
@@ -48,6 +48,7 @@ namespace Eden_Roleplay.ViewModel
         {
             get
             {
+                //If _updateAvailable is True, change the visibility of Update Available Button
                 return _updateAvailable ? System.Windows.Visibility.Visible : System.Windows.Visibility.Hidden;
             }
             set
@@ -59,6 +60,7 @@ namespace Eden_Roleplay.ViewModel
         {
             get
             {
+                //if version number is not the latest, display Update Available button next to version number
                 return (_updateAvailable) ? "Update Available" : "";
             }
             set
@@ -79,7 +81,9 @@ namespace Eden_Roleplay.ViewModel
             LaunchEdenCommand = new DelegateCommand(LaunchEden);
             QuitCommand = new DelegateCommand(Quit);
             UpdateCommand = new DelegateCommand(Update);
+            //Grab the version number tag from github json and set the variable
             Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            //Uncomment below to manually set version number
             //Version = "0.3b";
             LoadChangeLog();
             GetNextVersion();
@@ -88,24 +92,34 @@ namespace Eden_Roleplay.ViewModel
 
         private void Update()
         {
-            Process.Start(new ProcessStartInfo("https://https://github.com/Malankore/Eden-Roleplay-Launcher/releases/latest") { UseShellExecute = true });
+            Process.Start(new ProcessStartInfo("https://github.com/Malankore/Eden-Roleplay-Launcher/releases/latest") { UseShellExecute = true });
         }
 
         private void LoadChangeLog()
         {
+            //Changelog json base website
             var client = new RestClient("http://tjeynon.us/");
+            //Changelog json file and directory
             var request = new RestRequest("edenchanges.json", DataFormat.Json);
+            //Request function for changlog json
             var response = client.Get(request);
+            //Deserialize retrieved json
             var data = JsonConvert.DeserializeObject<List<Model.ChangeLog>>(response.Content);
+            //Load deserialized json data to a collection
             ChangeLog = new ObservableCollection<Model.ChangeLog>(data.OrderByDescending(o => o.Date));
         }
 
         private void GetNextVersion()
         {
+            //Version number json base website
             var client = new RestClient("https://github.com/Malankore/");
+            //Version number json directory
             var request = new RestRequest("Eden-Roleplay-Launcher/releases/latest");
+            //Request function for version json
             var response = client.Get(request);
+            //Deserialize Version Json
             var gr = JsonConvert.DeserializeObject<Model.GitRelease>(response.Content);
+            //If the version listed on github's latest does not match, display update available.
             if (gr.tag_name != Version)
             {
                 _updateAvailable = true;
@@ -116,21 +130,25 @@ namespace Eden_Roleplay.ViewModel
 
         private void Quit()
         {
+            //Close the application
             System.Windows.Application.Current.Shutdown();
         }
 
         private void LaunchEden()
         {
+            //launch fivem and connect to the server
             Process.Start(new ProcessStartInfo("fivem://connect/154.16.67.95:30120") { UseShellExecute = true });
         }
 
         private void OpenDiscord()
         {
+            //launch users default browser and launch the discord invite
             Process.Start(new ProcessStartInfo("https://discord.gg/fzexZJ4") { UseShellExecute = true });
         }
 
         private void ViewSite()
         {
+            //launch users default browser and open the website
             Process.Start(new ProcessStartInfo("https://discord.gg/fzexZJ4") { UseShellExecute = true });
         }
     }
